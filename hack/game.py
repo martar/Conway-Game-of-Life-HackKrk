2a0ee5026b12bd6011349c33da1de105a6a6f26c
Marta Rylko <rylko.marta@gmail.com>
2012-04-13 01:13:45 +0200

    Initial commit

---
+++ hack/game.py
@@ -0,0 +1,56 @@
+'''
+Created on 12-04-2012
+
+@author: Marta
+'''
+
+'''
+    The Game of Life, also known simply as Life, is a cellular automaton devised by the British 
+    mathematician John Horton Conway in 1970.[1]
+    The "game" is a zero-player game, meaning that its evolution is determined by its initial state,
+    requiring no further input. 
+    One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
+    
+    by Wikipedia
+'''
+
+class Board:
+    '''    
+    Board is a infinitive list of coordiantes. Active cells are stored in 
+    cells variable. A 'tick' moves the game state to the next stage in live
+    '''
+    
+    def __init__(self):
+        self.cells = []
+    
+    def gen_dict(self):
+        '''
+        Generates a neighbourship relations dictionary. Keys are tuples of coordiantes
+        '''
+        result = {}
+        for x, y in self.cells:
+            nbrs = [(x-1, y-1), (x, y-1), (x+1, y-1),
+                    (x-1, y),  (x+1, y),
+                    (x-1, y+1), (x, y+1), (x+1, y+1)]
+            for a, b in nbrs:
+                if result.has_key((a,b)):
+                    result[(a,b)] += 1
+                else:
+                    result[(a,b)] = 1
+        return result
+    
+    def should_live(self, count, active):
+        return (active and (count == 2)) or (count == 3)
+    
+    def tick(self):
+        '''
+        Method that moves to the next generation.
+        '''
+        new_cells = []
+        neighbour_relations = self.gen_dict()
+        for key, neighbour_count in neighbour_relations.iteritems():
+            active = key in self.cells
+            if self.should_live(neighbour_count, active):
+                new_cells.append(key)
+        self.cells = new_cells
+        return new_cells
---
+++ tests/test.py
@@ -0,0 +1,62 @@
+'''
+Created on 12-04-2012
+
+@author: Marta
+'''
+import unittest
+
+from hack import game
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.board = game.Board()
+        
+    def test_gen_dict(self):
+        self.board.cells = [(1,1), (3,2)]
+        result = self.board.gen_dict()
+        self.assertDictContainsSubset({(0,0):1,(1,0):1, (2,0):1, (3,1):1, (4,1):1, (4,2):1, 
+                                       (4,3):1, (3,3):1, (2,3):1, (1,2):1, (0,2):1, (0,1):1,
+                                        (2,1):2, (2,2):2 }, result)
+
+    def test_should_live_0_1(self):
+        result = self.board.should_live(0, True)
+        self.assertEqual(result, False)
+        result = self.board.should_live(1, True)
+        self.assertEqual(result, False)
+        
+    def test_should_live_2_3(self):
+        result = self.board.should_live(2, True)
+        self.assertEqual(result, True)
+        result = self.board.should_live(2, False)
+        self.assertEqual(result, False)
+        result = self.board.should_live(3, True)
+        self.assertEqual(result, True)
+        
+    def test_should_live_more_3(self):
+        result = self.board.should_live(4, True)
+        self.assertEqual(result, False)
+        result = self.board.should_live(8, True)
+        self.assertEqual(result, False)
+        
+    def test_tick(self):
+        self.board.cells = [(2,1), (2,2), (2,3)]
+        self.board.tick()
+        expected = [(1,2),(2,2), (3,2)]
+        expected.sort()
+        self.board.cells.sort()
+        self.assertListEqual(expected, self.board.cells)     
+
+    def test_tick_tok(self):
+        oscilator_input = [(2,1), (2,2), (2,3)]
+        oscilator_input.sort()
+        self.board.cells = oscilator_input
+        self.board.tick()
+        self.board.tick()
+        self.board.cells.sort()
+        self.assertListEqual(oscilator_input, self.board.cells)    
+                
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
